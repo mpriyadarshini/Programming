51b75c0fcc79b6ab902df63e29d0a7cff8b9ab4c
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-05-16 00:46:58 +0530

    Fix formatting

--- CheckPermutation.java
+++ CheckPermutation.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
-public class CheckPermutation{
-    public static boolean permutation(String s1,String s2){
+public class CheckPermutation {
+    public static boolean permutation(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-        if(n1!=n2)
-        return false;
+        if (n1 != n2)
+            return false;
         char c1[] = s1.toCharArray();
         char c2[] = s2.toCharArray();
         Arrays.sort(c1);
         Arrays.sort(c2);
-        for(int i = 0;i<n1;i++){
-            if(c1[i]!= c2[i])
-            return false;
+        for (int i = 0; i < n1; i++) {
+            if (c1[i] != c2[i])
+                return false;
         }
         return true;
 
     }
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str1 = s.next();
         String str2 = s.next();
-        if(permutation(str1,str2)){
+        if (permutation(str1, str2)) {
             System.out.println("yes");
-        }
-        else{
+        } else {
             System.out.println("no");
         }
 
