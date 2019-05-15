fb8c7b3a9ee9c9c4383b92b0716aeeeb80c12ff4
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-05-16 01:20:18 +0530

    Check IsUniqueAlternate

--- IsUniqueAlternate.java
+++ IsUniqueAlternate.java
@@ -2,17 +2,15 @@ import java.util.*;
 
 public class IsUniqueAlternate{
     public static boolean unique(String str){
-        boolean result = false;
+       boolean result = false;
         HashSet <Character> hm = new HashSet<>();
         for(int i=0;i<str.length();i++){
             result = hm.add(str.charAt(i));
-            if(result==false){
-                result=false;
-            }else{
-                result=true;
-            }
+            if(!result){
+                return false;
         }
-        return result;
+    }
+        return true;
     }
 
     public static void main(String[] args) {
